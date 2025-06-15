6dbfe34c4edaf7d75628a8c757cfab5054fbfac3
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2025-06-15 15:46:37 +0900

    Refactor background script and context menu handling; streamline extension startup logic and ensure context menu creation on installation

--- background.js
+++ background.js
@@ -22,17 +22,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
 // Note: chrome.action.onClicked is not used when default_popup is set in manifest
 // The popup will handle both settings and redirection logic
 
-// Extension startup handler
-chrome.runtime.onInstalled.addListener(function () {
-  // Check current active tab immediately
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (tabs[0] && tabs[0].url && tabs[0].url.match(/dev\.azure\.com.*\/_git\//)) {
-      chrome.action.setBadgeText({text:"ADO"});
-      chrome.action.setBadgeBackgroundColor({ color: [255, 51, 51, 255]});
-    }
-  });
-});
-
 function convertToGitHubUrl(adoUrl, adoOrg, githubOrg) {
   const urlPattern = /https:\/\/dev\.azure\.com\/([^\/]+)\/([^\/]+)\/_git\/([^\/]+)/;
   const match = adoUrl.match(urlPattern);
@@ -51,29 +40,42 @@ function convertToGitHubUrl(adoUrl, adoOrg, githubOrg) {
   return `https://github.com/${githubOrg}/${projectName}-${repoName}`;
 }
 
-// Context menu for quick access
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'redirectToGitHub',
-    title: 'Redirect to GitHub',
-    contexts: ['page'],
-    documentUrlPatterns: ['https://dev.azure.com/*']
+// Extension startup handler
+chrome.runtime.onInstalled.addListener(function () {
+  // Check current active tab immediately
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs[0] && tabs[0].url && tabs[0].url.match(/dev\.azure\.com.*\/_git\//)) {
+      chrome.action.setBadgeText({text:"ADO"});
+      chrome.action.setBadgeBackgroundColor({ color: [255, 51, 51, 255]});
+    }
   });
-});
-
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId === 'redirectToGitHub') {
-    const settings = await chrome.storage.sync.get(['adoOrg', 'githubOrg']);
-    
-    if (!settings.adoOrg || !settings.githubOrg) {
-      // Open popup if settings not configured
-      chrome.tabs.create({ url: chrome.runtime.getURL('popup.html') });
-      return;
-    }
-    
-    const githubUrl = convertToGitHubUrl(tab.url, settings.adoOrg, settings.githubOrg);
-    if (githubUrl) {
-      chrome.tabs.update(tab.id, { url: githubUrl });
-    }
+  
+  // Create context menu
+  if (chrome.contextMenus) {
+    chrome.contextMenus.create({
+      id: 'redirectToGitHub',
+      title: 'Redirect to GitHub',
+      contexts: ['page'],
+      documentUrlPatterns: ['https://dev.azure.com/*']
+    });
   }
 });
+
+if (chrome.contextMenus) {
+  chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (info.menuItemId === 'redirectToGitHub') {
+      const settings = await chrome.storage.sync.get(['adoOrg', 'githubOrg']);
+      
+      if (!settings.adoOrg || !settings.githubOrg) {
+        // Open popup if settings not configured
+        chrome.tabs.create({ url: chrome.runtime.getURL('popup.html') });
+        return;
+      }
+      
+      const githubUrl = convertToGitHubUrl(tab.url, settings.adoOrg, settings.githubOrg);
+      if (githubUrl) {
+        chrome.tabs.update(tab.id, { url: githubUrl });
+      }
+    }
+  });
+}
